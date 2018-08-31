ebeae06f6b515e6f60caaa0bca80643b5d7ae440
Sarah <sarahlynch@users.noreply.github.com>
2018-08-31 10:56:45 +1000

    remove the things to make it easier to see diff

--- pillest/__init__.py
+++
@@ -1,4 +0,0 @@
-
-
-class PillestError(Exception):
-    pass
--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='pillest',
-      version='0.1.0',
-      description='Image Manipulation and Generation',
-      author='Sarah Lynch',
-      author_email='sarahlynch@fastmail.com',
-      packages=['pillest'],
-     )
